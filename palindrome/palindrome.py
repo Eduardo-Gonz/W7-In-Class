8fc7d82456ec5c76c6cc0242af8c44b8be3e4011
Eduardo-Gonz <erugonz249@gmail.com>
2021-05-15 18:04:41 -0700

    Create palindrome.py and add skeletal code

---
+++ palindrome/palindrome.py
@@ -0,0 +1,13 @@
+def is_palindrome():
+
+
+    return 0
+
+
+
+def prompt_user():
+    userInput = input("Enter a string to see if it is a palindrome: ")
+    
+    is_palindrome(userInput)
+
+    return 0
