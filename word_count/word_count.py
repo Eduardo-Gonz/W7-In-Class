5d35e34a030e95d85ac704d2a624f65e27094954
Eduardo-Gonz <erugonz249@gmail.com>
2021-05-15 21:48:13 -0700

    Create word_count.py, a file that counts words from a string that is input by the user

---
+++ word_count/word_count.py
@@ -0,0 +1,15 @@
+def count_words(str):
+    wordCount = len(str.split())
+
+    return wordCount
+
+
+def prompt_user():
+    userInput = input("Enter a string to check how many words it has: ")
+
+    print(count_words(userInput))
+
+    return 0
+
+
+prompt_user()
